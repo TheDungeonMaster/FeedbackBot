453a1ae0a5e7c4f0d4e13b1ca04a2c9570be4601
abdi <abdiakhmet.1.618@gmail.com>
2024-02-06 23:15:52 +0600

    resolved token leak

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from telegram import Update
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
+import api_key
 
 # Replace 'YOUR_TOKEN_HERE' with your bot's token
-TOKEN = '6952588526:AAH3NQ-TECU2T3R1ltzkmalEB8YfzTBe25A'
+TOKEN = api_key.TOKEN
 
 # Replace 'GROUP_CHAT_ID' with the actual chat ID of your group
 GROUP_CHAT_ID = '-1002043872444'
@@ -21,20 +22,29 @@ async def forward_to_group(update: Update, context: ContextTypes.DEFAULT_TYPE) -
                                       message_id=update.message.message_id)
 
 
+async def reply_to_user(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Forward reply from the group to the original user who sent the message."""
+    global message_map
+    reply_to_message = update.message.reply_to_message
+
+    # Check if the message is a reply to a forwarded message
+    if reply_to_message and reply_to_message.message_id in message_map:
+        original_chat_id = message_map[reply_to_message.message_id]
+        # Send the reply to the original sender in a private chat
+        await context.bot.send_message(chat_id=original_chat_id,
+                                       text=f"Reply from group chat: {update.message.text}")
+
+
 def main() -> None:
     """Start the bot."""
-    # Create the Application and pass it your bot's token.
     application = Application.builder().token(TOKEN).build()
 
-    # on different commands - answer in Telegram
     application.add_handler(CommandHandler("start", start))
-
-    # on non-command i.e message - echo the message on Telegram
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, forward_to_group))
+    # Handler for messages, checks if the message is a reply
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND & filters.UpdateType.MESSAGE & filters.ChatType.GROUP, reply_to_user))
 
-    # Run the bot until the user presses Ctrl-C
     application.run_polling()
-
 
 if __name__ == '__main__':
     main()
